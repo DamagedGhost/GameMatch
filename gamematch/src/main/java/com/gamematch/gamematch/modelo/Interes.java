634e8c7f8b9763fef889fc28a639cb20c7bf9562
Joscelynne <josc.diaz@duocuc.cl>
2025-04-28 10:09:41 -0400

    Interes.java

---
+++ gamematch/src/main/java/com/gamematch/gamematch/modelo/Interes.java
@@ -0,0 +1,8 @@
+package com.gamematch.gamematch.modelo;
+
+
+public class Interes {
+        private int idInteres;           // Identificador único para el interés
+        private String nombreInteres;    // Nombre del interés (por ejemplo, "Música")
+
+}
