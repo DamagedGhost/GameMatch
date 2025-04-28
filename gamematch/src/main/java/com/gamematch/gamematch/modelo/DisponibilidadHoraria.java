a4126e3666e4219e5158db5788fbe62a97d1864f
Joscelynne <josc.diaz@duocuc.cl>
2025-04-28 10:11:25 -0400

    DisponibilidadHoraria.java

---
+++ gamematch/src/main/java/com/gamematch/gamematch/modelo/DisponibilidadHoraria.java
@@ -0,0 +1,11 @@
+package com.gamematch.gamematch.modelo;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class DisponibilidadHoraria {
+    private DayOfWeek dia;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+
+}
