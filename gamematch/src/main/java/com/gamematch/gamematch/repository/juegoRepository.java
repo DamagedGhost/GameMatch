786ffc1a1423c8e48ceff29eb8424e752b09b951
Ligthzz <joaquinmedina025@gmail.com>
2025-04-28 10:31:08 -0400

    juegoRepository.java

--- gamematch/src/main/java/com/gamematch/gamematch/repository/juegoRepository.java
+++ gamematch/src/main/java/com/gamematch/gamematch/repository/juegoRepository.java
@@ -3,9 +3,31 @@ package com.gamematch.gamematch.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class juegoCRUD {
-  // prueba
+public class juegoRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-  // prueba 2
+      public List<Juego> findAll() {
+        return entityManager.createQuery("SELECT * FROM juegos", Juego.class).getResultList(); //Listar toda la lista
+    }
 
-}
+    public Juego findById(Long id) { // Buscar por id
+        return entityManager.find(Juego.class, id);
+    }
+
+    public Juego save(Juego juego) {
+        if (juego.getId() == null) {
+            entityManager.persist(juego); // Agregar
+        } else {
+            juego = entityManager.merge(juego); // Actualizar
+        }
+        return juego;
+    }
+
+    public void delete(Long id) { //borrar por id
+        Juego juego = findById(id);
+        if (juego != null) {
+            entityManager.remove(juego);
+        }
+    }
+} 
