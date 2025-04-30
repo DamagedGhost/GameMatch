203c43479a8865ccefc8664f5132900f565e52e1
Joscelynne <josc.diaz@duocuc.cl>
2025-04-30 10:06:39 -0400

    Interes.java

--- gamematch/src/main/java/com/gamematch/gamematch/modelo/Interes.java
+++ gamematch/src/main/java/com/gamematch/gamematch/modelo/Interes.java
@@ -1,5 +1,15 @@
 package com.gamematch.gamematch.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data // anotacion de Lombok para generar getters y setters
+@AllArgsConstructor // anotacion de Lombok para generar constructor con todos los atributos
+@NoArgsConstructor // anotacion de Lombok para generar constructor sin argumentos
+
 
 public class Interes {
         @SuppressWarnings("unused")
