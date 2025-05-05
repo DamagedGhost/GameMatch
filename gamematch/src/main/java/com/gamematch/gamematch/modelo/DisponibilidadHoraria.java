ca0b14daa274bae68055f9e3f8e59e21023b740d
Joscelynne <josc.diaz@duocuc.cl>
2025-05-04 21:33:08 -0400

    perfil crud

--- gamematch/src/main/java/com/gamematch/gamematch/modelo/DisponibilidadHoraria.java
+++ gamematch/src/main/java/com/gamematch/gamematch/modelo/DisponibilidadHoraria.java
@@ -3,8 +3,16 @@ package com.gamematch.gamematch.modelo;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data // anotacion de Lombok para generar getters y setters
+@AllArgsConstructor
+@NoArgsConstructor
 public class DisponibilidadHoraria {
-    @SuppressWarnings("unused")
+
+    // atributos de la clase Disponibilidad
+    //@SuppressWarnings("unused")
     private DayOfWeek dia;
     @SuppressWarnings("unused")
     private LocalTime horaInicio;
